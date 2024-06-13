622e0c291198ebff1d81de298604212c7e5404e9
yezin.kim <yezin.kim@samsung.com>
2024-06-13 09:56:22 +0900

    [feature] 37

--- prime_factors.py
+++ prime_factors.py
@@ -2,5 +2,10 @@ class PrimeFactor:
     def of(self, number) -> []:
         factors = []
         if number > 1:
-            factors.append(number)
+            if number == 4:
+                while number % 2 == 0:
+                    factors.append(2)
+                    number //= 2
+            else:
+                factors.append(number)
         return factors
